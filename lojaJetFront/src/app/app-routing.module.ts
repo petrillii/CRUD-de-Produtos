364618ddd3c25a4f63de3c9ba0229c29e73108c9
Eduardo <eduardo.petrilli@mag-it.com.br>
2022-06-26 19:00:38 -0300

    Config structure

--- lojaJetFront/src/app/app-routing.module.ts
+++ lojaJetFront/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListItemsComponent } from './components/list-items/list-items.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ListItemsComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lojaJetFront/src/app/app.module.ts
+++ lojaJetFront/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListItemsComponent } from './components/list-items/list-items.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
